328e75c68682ccfbda95fef909d574c5f77bf2f9
GhostWarrior25 <luidbrav@espol.edu.ec>
2021-10-19 15:17:06 -0500

    se agregan variables altura, peso, IMC que guardan los datos de usuario y permiten realizar las operaciones \n se realiza la operarion de calcular el imc, revisar mayor y agregarlo a la sumatoria, que posterior se divide para obtener el promedio \n se realiza revision de errores

--- main.c
+++ main.c
@@ -21,13 +21,38 @@ int main(int argc, char **argv){
 				exit(1);
 		}
 	}
-
+	
 	//Los resultados de sus calculos van en estas variables.
 	//Puede declarar más variables si lo necesita.
 	float sum = 0.0f;
 	float max_imc = 0.0f;
-	
-	
+	float peso = 0.00f;
+	float altura = 0.0f;
+	float IMC = 0.0f;
+	float prom = 0.0f;
+	int i = 0;
+	for (i; i<elementos;i++){
+		printf("Peso: ");
+		scanf("%.2f", &peso);
+		if (peso < 0){
+			printf("\nNumero Invalido");
+			exit(1);
+		}
+		printf("\n Altura: ");
+		scanf("%.2f", &altura);
+		if (altura < 0){
+			printf("\nNumero Invalido");
+			exit(1);
+		}
+		IMC = (peso /(altura**2));
+		sum += IMC;
+		if (IMC > max_imc){
+			max_imc = IMC;
+		}
+	}
+	sum = sum / elementos;
+
+
 	
 	
 	//No modifique estas lineas
@@ -36,6 +61,7 @@ int main(int argc, char **argv){
 	printf("\npromedio IMC: %.1f\n", sum);
 	printf("maximo IMC: %.1f\n", max_imc);
 	//linea prueba
+	
 	
 }
 
